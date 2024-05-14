d4e14d5c73128623a9aa06022402dbf0e0933790
Mariano D'Angelo <mariano.dangelo@claro.com.ar>
2024-05-14 20:08:50 -0300

    fix ui

--- HospitalUI/Form1.Designer.cs
+++ HospitalUI/Form1.Designer.cs
@@ -28,106 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            grb_persMed = new System.Windows.Forms.GroupBox();
-            lsb_medicos = new System.Windows.Forms.ListBox();
-            grb_pacientes = new System.Windows.Forms.GroupBox();
-            lsb_pacientes = new System.Windows.Forms.ListBox();
-            btn_atender = new System.Windows.Forms.Button();
-            btn_salir = new System.Windows.Forms.Button();
-            rtxb_info = new System.Windows.Forms.RichTextBox();
+            grb_persMed = new GroupBox();
+            lstMedicos = new ListBox();
+            grb_pacientes = new GroupBox();
+            lstPacientes = new ListBox();
+            btnAtender = new Button();
+            btnSalir = new Button();
+            rtbInfoMedicos = new RichTextBox();
             grb_persMed.SuspendLayout();
             grb_pacientes.SuspendLayout();
-            this.SuspendLayout();
+            SuspendLayout();
             // 
             // grb_persMed
             // 
-            grb_persMed.Controls.Add(lsb_medicos);
-            grb_persMed.Location = new System.Drawing.Point(2, 30);
+            grb_persMed.Controls.Add(lstMedicos);
+            grb_persMed.Location = new Point(2, 30);
             grb_persMed.Name = "grb_persMed";
-            grb_persMed.Size = new System.Drawing.Size(473, 197);
+            grb_persMed.Size = new Size(473, 197);
             grb_persMed.TabIndex = 0;
             grb_persMed.TabStop = false;
             grb_persMed.Text = "Personal Medico";
             // 
-            // lsb_medicos
+            // lstMedicos
             // 
-            lsb_medicos.FormattingEnabled = true;
-            lsb_medicos.ItemHeight = 19;
-            lsb_medicos.Location = new System.Drawing.Point(6, 25);
-            lsb_medicos.Name = "lsb_medicos";
-            lsb_medicos.Size = new System.Drawing.Size(436, 156);
-            lsb_medicos.TabIndex = 0;
+            lstMedicos.FormattingEnabled = true;
+            lstMedicos.ItemHeight = 19;
+            lstMedicos.Location = new Point(6, 25);
+            lstMedicos.Name = "lstMedicos";
+            lstMedicos.Size = new Size(436, 156);
+            lstMedicos.TabIndex = 0;
             // 
             // grb_pacientes
             // 
-            grb_pacientes.Controls.Add(lsb_pacientes);
-            grb_pacientes.Location = new System.Drawing.Point(497, 33);
+            grb_pacientes.Controls.Add(lstPacientes);
+            grb_pacientes.Location = new Point(497, 33);
             grb_pacientes.Name = "grb_pacientes";
-            grb_pacientes.Size = new System.Drawing.Size(498, 201);
+            grb_pacientes.Size = new Size(498, 201);
             grb_pacientes.TabIndex = 1;
             grb_pacientes.TabStop = false;
             grb_pacientes.Text = "Pacientes";
             // 
-            // lsb_pacientes
+            // lstPacientes
             // 
-            lsb_pacientes.FormattingEnabled = true;
-            lsb_pacientes.ItemHeight = 19;
-            lsb_pacientes.Location = new System.Drawing.Point(21, 28);
-            lsb_pacientes.Name = "lsb_pacientes";
-            lsb_pacientes.Size = new System.Drawing.Size(460, 156);
-            lsb_pacientes.TabIndex = 0;
+            lstPacientes.FormattingEnabled = true;
+            lstPacientes.ItemHeight = 19;
+            lstPacientes.Location = new Point(21, 28);
+            lstPacientes.Name = "lstPacientes";
+            lstPacientes.Size = new Size(460, 156);
+            lstPacientes.TabIndex = 0;
             // 
-            // btn_atender
+            // btnAtender
             // 
-            btn_atender.Location = new System.Drawing.Point(1062, 79);
-            btn_atender.Name = "btn_atender";
-            btn_atender.Size = new System.Drawing.Size(160, 83);
-            btn_atender.TabIndex = 2;
-            btn_atender.Text = "Atender";
-            btn_atender.UseVisualStyleBackColor = true;
+            btnAtender.Location = new Point(1062, 79);
+            btnAtender.Name = "btnAtender";
+            btnAtender.Size = new Size(160, 83);
+            btnAtender.TabIndex = 2;
+            btnAtender.Text = "Atender";
+            btnAtender.UseVisualStyleBackColor = true;
+            btnAtender.Click += btnAtender_Click;
             // 
-            // btn_salir
+            // btnSalir
             // 
-            btn_salir.Location = new System.Drawing.Point(1175, 402);
-            btn_salir.Name = "btn_salir";
-            btn_salir.Size = new System.Drawing.Size(91, 82);
-            btn_salir.TabIndex = 3;
-            btn_salir.Text = "Salir";
-            btn_salir.UseVisualStyleBackColor = true;
+            btnSalir.Location = new Point(1175, 402);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(91, 82);
+            btnSalir.TabIndex = 3;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
             // 
-            // rtxb_info
+            // rtbInfoMedicos
             // 
-            rtxb_info.Location = new System.Drawing.Point(15, 244);
-            rtxb_info.Name = "rtxb_info";
-            rtxb_info.Size = new System.Drawing.Size(990, 286);
-            rtxb_info.TabIndex = 4;
-            rtxb_info.Text = "";
+            rtbInfoMedicos.Location = new Point(15, 244);
+            rtbInfoMedicos.Name = "rtbInfoMedicos";
+            rtbInfoMedicos.Size = new Size(990, 286);
+            rtbInfoMedicos.TabIndex = 4;
+            rtbInfoMedicos.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1278, 542);
-            this.Controls.Add(rtxb_info);
-            this.Controls.Add(btn_salir);
-            this.Controls.Add(btn_atender);
-            this.Controls.Add(grb_pacientes);
-            this.Controls.Add(grb_persMed);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1278, 542);
+            Controls.Add(rtbInfoMedicos);
+            Controls.Add(btnSalir);
+            Controls.Add(btnAtender);
+            Controls.Add(grb_pacientes);
+            Controls.Add(grb_persMed);
+            Name = "Form1";
+            Text = "Mariano DAngelo";
             grb_persMed.ResumeLayout(false);
             grb_pacientes.ResumeLayout(false);
-            this.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
 
         private GroupBox grb_persMed;
-        private ListBox lsb_medicos;
+        private ListBox lstMedicos;
         private GroupBox grb_pacientes;
-        private ListBox lsb_pacientes;
-        private Button btn_atender;
-        private Button btn_salir;
-        private RichTextBox rtxb_info;
+        private ListBox lstPacientes;
+        private Button btnAtender;
+        private Button btnSalir;
+        private RichTextBox rtbInfoMedicos;
     }
 }
--- HospitalUI/Form1.cs
+++ HospitalUI/Form1.cs
@@ -1,10 +1,19 @@
+using System.Diagnostics;
+
 namespace HospitalUI
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
+        }
+
+        private void btnAtender_Click(object sender, EventArgs e) {
+            Debug.WriteLine("BTN_ATENDER_CLICK");
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e) {
+            Debug.WriteLine("BTN_SALIR_CLICK");
         }
     }
 }
